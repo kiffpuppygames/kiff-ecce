1d737ba0ad3767897f8d5dab7683b96f5a037df2
chaosratel <138471433+kiffpuppygames@users.noreply.github.com>
2025-03-20 16:18:37 +0000

    added common lib; and update build config

--- build.zig
+++ build.zig
@@ -4,9 +4,36 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    const kiff_common = b.dependency("kiff_common", .{
+        .target = target,
+        .optimize = optimize,
+        
+    });
+
     _ = b.addModule("kiff_ecce", .{
         .target = target,
         .optimize = optimize,
         .root_source_file = b.path("src/root.zig"),
     });
+
+    const exe_mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "kiff_ecce",
+        .root_module = exe_mod,
+    });
+    exe.root_module.addImport("kiff_common", kiff_common.module("kiff_common"));
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
 }
